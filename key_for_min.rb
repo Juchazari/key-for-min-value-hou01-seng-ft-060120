f612cc0f65885247cd9b53cce9127c40309a0d89
Julio Chazari <julio.chazari@gmail.com>
2020-05-21 02:57:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  
+  #reduces the hash into an array [:key, value]
+  min_value_key = name_hash.reduce do |key, value|
+    key.last > value.last ? value : key
+  end
+  if name_hash == {}
+    lowest_key
+  else
+    lowest_key.first
+  end
 end
