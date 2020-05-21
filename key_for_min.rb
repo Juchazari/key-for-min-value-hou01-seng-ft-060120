4baf59bbd1e9c07668f65fb6168ba3178d52ea45
Julio Chazari <julio.chazari@gmail.com>
2020-05-21 02:58:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,12 @@
 def key_for_min_value(name_hash)
   
   min_value_key = name_hash.reduce do |key, value|
-    key.last > value.last ? value : key
+    if key.last > value.last
+      value
+    else
+      key
+    end
+    # key.last > value.last ? value : key
   end
   
   if name_hash == {}
